169713bbba74bbe9cdafde83040fbae9614bc30f
demetriotorgan <demetriotorgan@hotmail.com>
2025-12-03 11:41:20 -0300

    excluirAbastecimento

--- src/componentes/Abastecimento.jsx
+++ src/componentes/Abastecimento.jsx
@@ -8,7 +8,8 @@ import ModalCarregamento from '../componentes/ModalCarregamento';
 import { useSalvarAbastecimento } from '../hooks/useSalvarAbastecimento';
 
 const Abastecimento = () => {
-  
+  const [excluindo, setExcluindo] = useState(false);
+
   const {
       data: list,
       loading,
@@ -22,9 +23,34 @@ const Abastecimento = () => {
         handleDadosAbastecimento,
         salvarAbastecimento
     } = useSalvarAbastecimento({setList});
+
+  const handleExcluir = async(item)=>{
+    // ▶️ 1. Verificação imediata OFFLINE
+      if (!navigator.onLine) {
+        alert(
+          "❌ Você está offline.\nA exclusão só pode ser realizada quando a conexão estiver ativa."
+        );
+        return;
+      }
+  const confirmar = window.confirm('Deseja realmente excluir este registro?');
+  if(!confirmar) return
+
+  try {
+    setExcluindo(true)
+    const response = await api.delete(`/deletar-abastecimento/${item._id}`);
+    console.log(response.data);
+    alert('Regsitro excluido com sucesso');
+    setList((prev) => prev.filter((p) => p._id !== item._id));
+  } catch (error) {
+    console.log(error);
+  }finally{
+    setExcluindo(false);
+  }
+
+  }
   return (
     <>
-    {salvando && (<ModalCarregamento label='Salvando' />)}
+    {(salvando || excluindo) && (<ModalCarregamento label={salvando ? 'Salvando' : 'Excluindo'} />)}
     <div className='container'>
       <h2>Novo Abastecimento</h2>
       <label>
