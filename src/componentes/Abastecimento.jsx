07c1543c8da6859e24a21e3c37f08a77bc73b23c
demetriotorgan <demetriotorgan@hotmail.com>
2025-12-03 11:28:24 -0300

    useSalvarPedagioFinal

--- src/componentes/Abastecimento.jsx
+++ src/componentes/Abastecimento.jsx
@@ -5,20 +5,10 @@ import { Save, Trash2 } from "lucide-react";
 import api from '../api/api';
 import { useEntityList } from '../hooks/useEntityList';
 import ModalCarregamento from '../componentes/ModalCarregamento';
+import { useSalvarAbastecimento } from '../hooks/useSalvarAbastecimento';
 
 const Abastecimento = () => {
-  const abastecimentoInicial = {
-    local: '',
-    valor: '',
-    litros: '',
-    odometro: '',
-    valorLitro: '',
-    data:''
-  }
-
-  const [dadosAbastecimento, setDadosAbastecimento] = useState(abastecimentoInicial);
-  const [salvando, setSalvando] = useState(false);
-
+  
   const {
       data: list,
       loading,
@@ -26,55 +16,12 @@ const Abastecimento = () => {
       refresh: reload
     } = useEntityList("abastecimentos");
 
-  const handleDadosAbastecimento = (e)=>{
-    const {name, value} = e.target;
-    setDadosAbastecimento((prev) => ({...prev, [name]:value}));
-  };
-
-  const criarPayload = () => ({
-    local: dadosAbastecimento.local,
-    valor: dadosAbastecimento.valor,
-    litros: dadosAbastecimento.litros,
-    odometro: dadosAbastecimento.odometro,
-    valorLitro: dadosAbastecimento.valorLitro,
-    data: dateToIso(dadosAbastecimento.data)
-  });
-
-  const salvarAbastecimento = async() =>{
-    if(!window.confirm('Deja salvar este abastecimento?')) return;
-
-    try {
-      setSalvando(true);
-
-      const payload = criarPayload();
-      const response = await api.post('/salvar-abastecimento', payload);
-
-      if(!response.data.offline){
-        setList((prev) => [response.data.abastecimento, ...prev]);
-        alert('Abastecimento salvo com sucesso!');
-        setDadosAbastecimento(abastecimentoInicial);
-        return;
-      }
-
-      const tempItem = {
-        ...payload,
-        _id: "temp-" + Date.now(),
-        offline:true,
-      };
-
-      setList((prev) => [tempItem, ...prev]);
-      alert('Sem conexão! O abastecimento foi salvo offline;');
-      setDadosAbastecimento(abastecimentoInicial);
-
-    } catch (error) {
-      console.error('Erro ao salvar abastecimento: ', error);
-      alert('Erro ao salvar Abastecimento');      
-    }finally{
-      setSalvando(false);
-    }
-
-  }
-
+  const {
+        dadosAbastecimento,
+        salvando,
+        handleDadosAbastecimento,
+        salvarAbastecimento
+    } = useSalvarAbastecimento({setList});
   return (
     <>
     <div className='container'>
